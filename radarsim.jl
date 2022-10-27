78a8dc04abd6a8494ec598c6d345909561def365
Mitchell Jacobs <mrjacobs@wpi.edu>
2022-10-27 20:16:41 +0000

    use modules

--- radarsim.jl
+++ radarsim.jl
@@ -1,73 +1,81 @@
 
-struct Target
-    range::Int          #Meters
-    attenuation::Int    #dB
-end
 
-FSPL(d,f) = 20*log10(d) + 20*log10(f) + 20*log10(4π/c)  #Free Space Path Loss 
-POW(sig) = 10*log10(sum(abs.(sig).^2)/length(sig));     #Power of complex signal
+module radarsim
 
-function single_pulse(samp_rate, prf, τ, β; target_pow = 0, envelope_type="Rectangular", pulse_type="Increasing")
-    prt = 1/prf;
-    t = 1/samp_rate:1/samp_rate:prt;
+    export Target;
+    export POW;
+    export gen_pulse_train, sim_return, single_pulse;
 
-    if envelope_type == "Rectangular"
-        a = ones(length(t));
-        if(length(a) > τ * samp_rate)
-            a[Int(τ*samp_rate):end] .= 0;
+    struct Target
+        range::Int          #Meters
+        attenuation::Int    #dB
+    end
+
+    FSPL(d,f) = 20*log10(d) + 20*log10(f) + 20*log10(4π/c)  #Free Space Path Loss 
+    POW(sig) = 10*log10(sum(abs.(sig).^2)/length(sig));     #Power of complex signal
+
+    function single_pulse(samp_rate, prf, τ, β; target_pow = 0, envelope_type="Rectangular", pulse_type="Increasing")
+        prt = 1/prf;
+        t = 1/samp_rate:1/samp_rate:prt;
+
+        if envelope_type == "Rectangular"
+            a = ones(length(t));
+            if(length(a) > τ * samp_rate)
+                a[Int(τ*samp_rate):end] .= 0;
+            end
+        elseif envelope_type == "Gaussian"
+            a = exp.((-t.^2)/(τ^2)) #Gaussian
         end
-    elseif envelope_type == "Gaussian"
-        a = exp.((-t.^2)/(τ^2)) #Gaussian
+
+        if pulse_type == "Increasing"
+            pulse = a.*exp.(im*π*(β/τ)*t.^2)
+        elseif pulse_type == "Decreasing"
+            pulse = a.*exp.(-im*π*β/τ*(t.^2 - 2*τ.*t))
+        end
+
+        pulse = pulse * 10^((target_pow - POW(pulse))/20)
+
+        return t, pulse;
     end
 
-    if pulse_type == "Increasing"
-        pulse = a.*exp.(im*π*(β/τ)*t.^2)
-    elseif pulse_type == "Decreasing"
-        pulse = a.*exp.(-im*π*β/τ*(t.^2 - 2*τ.*t))
+    function gen_pulse_train(repetitions, samp_rate, prf, τ, β; envelope_type="Rectangular", pulse_type="Increasing")
+        pulse = Array{ComplexF64}(undef,Int(round(1/prf * repetitions * samp_rate)));
+        t, p = single_pulse(samp_rate, prf, τ, β, envelope_type=envelope_type, pulse_type=pulse_type);
+
+        for i in 0:repetitions-1
+            idx = i*length(t)
+            pulse[idx+1:idx+length(t)] = p;
+        end
+
+        t = 1/samp_rate:1/samp_rate:1/prf*repetitions;
+        return t, pulse;
     end
 
-    pulse = pulse * 10^((target_pow - POW(pulse))/20)
+    function sim_return(samp_rate, pulse_train, targets::Array{Target}; system_loss=6, Gt=20, Gr=20, SNR = 100)
+        rx_pulse_train = zeros(ComplexF64, length(pulse_train))
+        for i in 1:num_targets
+            range = targets[i].range
 
-    return t, pulse;
-end
-
-function gen_pulse_train(repetitions, samp_rate, prf, τ, β; envelope_type="Rectangular", pulse_type="Increasing")
-    pulse = Array{ComplexF64}(undef,Int(round(1/prf * repetitions * samp_rate)));
-    t, p = single_pulse(samp_rate, prf, τ, β, envelope_type=envelope_type, pulse_type=pulse_type);
-
-    for i in 0:repetitions-1
-        idx = i*length(t)
-        pulse[idx+1:idx+length(t)] = p;
-    end
-
-    t = 1/samp_rate:1/samp_rate:1/prf*repetitions;
-    return t, pulse;
-end
-
-function sim_return(samp_rate, pulse_train, targets::Array{Target}; system_loss=6, Gt=20, Gr=20, SNR = 100)
-    rx_pulse_train = zeros(ComplexF64, length(pulse_train))
-    for i in 1:num_targets
-        range = targets[i].range
-
-        pathloss = FSPL(range*2,fc);
-        total_attenuation = pathloss - Gt - Gr + system_loss + targets[i].attenuation;
-        rx_pow = POW(pulse_train) - total_attenuation;
-        target_pulse_train = pulse_train * 10^((rx_pow - POW(pulse_train))/20)    
+            pathloss = FSPL(range*2,fc);
+            total_attenuation = pathloss - Gt - Gr + system_loss + targets[i].attenuation;
+            rx_pow = POW(pulse_train) - total_attenuation;
+            target_pulse_train = pulse_train * 10^((rx_pow - POW(pulse_train))/20)    
+            
+            
+            t_delay = (range * 2) / c;
+            ϕ = ((t_delay % (1/samp_rate)) / (1/samp_rate)) * 2π;
+            offset = Int(t_delay ÷ (1/samp_rate));
+            target_pulse_train = abs.(target_pulse_train) .* exp.(im*(angle.(target_pulse_train) .+ ϕ))
+            
+            rx_pulse_train[offset+1:end] = rx_pulse_train[offset+1:end] .+ target_pulse_train[begin:end-offset] #TODO: Why +1        
+        end
         
+        noise = rand(ComplexF64, length(rx_pulse_train));        
+        noise_pwr = POW(rx_pulse_train) - SNR;
+        noise = noise * 10^((noise_pwr - POW(noise)/20))
         
-        t_delay = (range * 2) / c;
-        ϕ = ((t_delay % (1/samp_rate)) / (1/samp_rate)) * 2π;
-        offset = Int(t_delay ÷ (1/samp_rate));
-        target_pulse_train = abs.(target_pulse_train) .* exp.(im*(angle.(target_pulse_train) .+ ϕ))
-        
-        rx_pulse_train[offset+1:end] = rx_pulse_train[offset+1:end] .+ target_pulse_train[begin:end-offset] #TODO: Why +1        
-    end
-    
-    noise = rand(ComplexF64, length(rx_pulse_train));        
-    noise_pwr = POW(rx_pulse_train) - SNR;
-    noise = noise * 10^((noise_pwr - POW(noise)/20))
-    
-    rx_pulse_train = rx_pulse_train .+ noise;
+        rx_pulse_train = rx_pulse_train .+ noise;
 
-    return rx_pulse_train
+        return rx_pulse_train
+    end
 end
